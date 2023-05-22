c89f4da6d6fded77d7534b28564518bcf978b7cb
Alex Seif <alex.seif@gmail.com>
2023-05-23 01:13:37 +0300

    daily md into html

---
+++ script.js
@@ -0,0 +1,13 @@
+axios.get('docs/daily.md')
+    .then(function(response) {
+        var ul = document.querySelector('ul');
+        var lines = response.data.split('\n');
+        lines.forEach(function(line) {
+            var li = document.createElement('li');
+            li.textContent = line;
+            ul.appendChild(li);
+        });
+    })
+    .catch(function(error) {
+        console.error('Error fetching daily.md:', error);
+    });
