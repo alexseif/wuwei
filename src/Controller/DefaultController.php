00323902998897e97af67348e10bc0cda0d7e9e2
Alex Seif <alex.seif@gmail.com>
2023-07-25 19:03:07 +0300

    Adding docs

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
