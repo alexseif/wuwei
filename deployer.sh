544d409ba8ce8f2fca57dea355857e2849185841
Alex Seif <alex.seif@gmail.com>
2023-07-26 13:08:09 +0300

    composer and deployer updates

--- deployer.sh
+++ deployer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git pull origin master
+composer install --no-dev --optimize-autoloader
 php8.2 bin/console cache:clear --env=prod
 php8.2 bin/console doctrine:migrations:migrate --no-interaction --env=prod
