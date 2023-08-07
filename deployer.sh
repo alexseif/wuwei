ceca457a519ac3b5c4f1fadc7b2a26c1ccdfa189
Alex Seif <alex.seif@gmail.com>
2023-08-07 16:44:40 +0300

    deplyer fix

--- deployer.sh
+++ deployer.sh
@@ -4,5 +4,5 @@ php8.2 /usr/local/bin/composer dump-env prod
 php8.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
 APP_ENV=prod APP_DEBUG=0 php8.2 bin/console cache:clear --env=prod
 php8.2 bin/console doctrine:migrations:migrate --no-interaction --env=prod
-ysrn install
+yarn install
 ./node_modules/.bin/encore production
