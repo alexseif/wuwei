707aeb9bdad4393b2587d2144e194e245633517b
Alex Seif <alex.seif@gmail.com>
2023-08-10 12:05:33 +0300

    app_dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -13,19 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
 
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_dashboard')]
     public function home(
       ItemRepository $itemRepository,
       ItemListRepository $itemListRepository,
       DailyRepository $dailyRepository
     ): Response {
-        $itemLists = $itemListRepository->findAllWithItems();
-        $lastDay = $dailyRepository->getLastDaily();
         return $this->render(
           'dashboard/dashboard.html.twig',
           [
-            'itemLists' => $itemLists,
-            'lastDay' => $lastDay,
+            'itemLists' => $itemListRepository->findAllWithItems(),
+            'lastDay' => $dailyRepository->getLastDaily(),
           ]
         );
     }
