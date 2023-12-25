0a5abca2a8dfa5548a48176c49503643ff96f2cf
Alex Seif <alex.seif@gmail.com>
2023-12-25 07:25:44 +0200

    Tag Type, Tag and taggableTrait

--- src/Entity/Traits/TaggableTrait.php
+++ src/Entity/Traits/TaggableTrait.php
@@ -2,35 +2,44 @@
 
 namespace App\Entity\Traits;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use App\Entity\Tag;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 trait TaggableTrait
 {
 
-
-    //#[ORM\JoinTable(name: "entity_tags", joinColumns: [#[ORM\JoinColumn(name: "entity_id", referencedColumnName: "id")]], inverseJoinColumns: [#[ORM\JoinColumn(name: "tag_id", referencedColumnName: "id")]])]
-    #[ORM\ManyToMany(targetEntity: Tag::class)]
-    #[ORM\JoinTable(name: "entity_tags",
-      joinColumns: [ORM\JoinColumn(name: 'entity_id', referencedColumnName: 'id'),],
-      inverseJoinColumns: [ORM\JoinColumn(name: 'tag_id', referencedColumnName: 'id')]
-    )]
-    private ArrayCollection|Tag[] $tags;
+    /**
+     * @var ArrayCollection|Tag[]
+     *
+     * @ORM\ManyToMany(targetEntity="Tag")
+     * @ORM\JoinTable(
+     *     name="entity_tags",
+     *     joinColumns={@ORM\JoinColumn(name="entity_id", referencedColumnName="id")},
+     *     inverseJoinColumns={@ORM\JoinColumn(name="tag_id", referencedColumnName="id")}
+     * )
+     */
+    private $tags;
 
     public function __construct()
     {
         $this->tags = new ArrayCollection();
     }
 
-    #[ORM\return(ArrayCollection|Tag[])]
-    public function getTags(): ArrayCollection|Tag[]
+    /**
+     * @return ArrayCollection|Tag[]
+     */
+    public function getTags()
     {
         return $this->tags;
     }
 
-    #[ORM\param(Tag)]
-    public function addTag(Tag $tag): static
+    /**
+     * @param Tag $tag
+     *
+     * @return $this
+     */
+    public function addTag(Tag $tag)
     {
         if (!$this->tags->contains($tag)) {
             $this->tags[] = $tag;
@@ -39,21 +48,28 @@ trait TaggableTrait
         return $this;
     }
 
-    #[ORM\param(Tag)]
-    public function removeTag(Tag $tag): static
+    /**
+     * @param Tag $tag
+     *
+     * @return $this
+     */
+    public function removeTag(Tag $tag)
     {
         $this->tags->removeElement($tag);
 
         return $this;
     }
 
-    #[ORM\param(ArrayCollection | Tag[])]
-    public function setTags(ArrayCollection|Tag[] $tags): static
+    /**
+     * @param ArrayCollection|Tag[] $tags
+     *
+     * @return $this
+     */
+    public function setTags($tags)
     {
         $this->tags = $tags;
 
         return $this;
     }
-
 
 }
