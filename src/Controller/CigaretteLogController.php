be92b5ba333ce3ac5e06feb5a9aa1fe86a4df6d2
Alex Seif <alex.seif@gmail.com>
2024-04-19 09:49:07 +0200

    Cigarette add access and ux

--- src/Controller/CigaretteLogController.php
+++ src/Controller/CigaretteLogController.php
@@ -17,9 +17,9 @@ class CigaretteLogController extends AbstractController
     #[Route('/', name: 'app_cigarette_log_index', methods: ['GET'])]
     public function index(CigaretteLogRepository $cigaretteLogRepository
     ): Response {
-
         return $this->render('cigarette_log/index.html.twig', [
-          'cigarette_logs' => $cigaretteLogRepository->findAll(),
+          'cigarette_logs' => $cigaretteLogRepository->findBy([],
+            ['createdAt' => 'DESC']),
           'cigarette_counts' => $cigaretteLogRepository->countByDay(),
         ]);
     }
@@ -33,13 +33,13 @@ class CigaretteLogController extends AbstractController
 
         $entityManager->persist($cigaretteLog);
         $entityManager->flush();
-
-        return $this->redirectToRoute(
-          'app_cigarette_log_index',
-          [],
-          Response::HTTP_SEE_OTHER
-        );
-
+        //Fetch the request referrer
+        $referer = $request->headers->get('referer');
+        //Get the symfony path for the referrer
+        $path = parse_url($referer, PHP_URL_PATH);
+        //Redirect to the referrer
+        return $this->redirect($path, Response::HTTP_SEE_OTHER);
+        //        return $this->redirectToRoute('app_cigarette_log_index', [], Response::HTTP_SEE_OTHER);
     }
 
     #[Route('/{id}', name: 'app_cigarette_log_show', methods: ['GET'])]
