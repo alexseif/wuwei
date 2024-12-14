c02db331a09846350dcd232f90a30d99cc2a728a
Alex Seif <alex.seif@gmail.com>
2024-12-14 17:24:51 +0200

    build: deployer local and npm package-lock

--- deployer-local.sh
+++ deployer-local.sh
@@ -4,5 +4,5 @@ php8.2 /usr/local/bin/composer dump-env dev
 php8.2 /usr/local/bin/composer install
 APP_ENV=dev APP_DEBUG=1 php8.2 bin/console cache:clear --env=dev
 php8.2 bin/console doctrine:migrations:migrate --no-interaction --env=dev
-yarn install
+npm install
 ./node_modules/.bin/encore dev
