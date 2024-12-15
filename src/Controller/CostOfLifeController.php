183221fde252bfccb2fdc70ef9a6639a39bd9b80
Alex Seif <alex.seif@gmail.com>
2024-12-15 14:08:16 +0200

    chore(Cost Of Life): crud

---
+++ src/Controller/CostOfLifeController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CostOfLife;
+use App\Form\CostOfLifeType;
+use App\Repository\CostOfLifeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/cost-of-life')]
+final class CostOfLifeController extends AbstractController
+{
+    #[Route(name: 'app_cost_of_life_index', methods: ['GET'])]
+    public function index(CostOfLifeRepository $costOfLifeRepository): Response
+    {
+        return $this->render('cost_of_life/index.html.twig', [
+            'cost_of_lives' => $costOfLifeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_cost_of_life_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $costOfLife = new CostOfLife();
+        $form = $this->createForm(CostOfLifeType::class, $costOfLife);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($costOfLife);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cost_of_life_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('cost_of_life/new.html.twig', [
+            'cost_of_life' => $costOfLife,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_cost_of_life_show', methods: ['GET'])]
+    public function show(CostOfLife $costOfLife): Response
+    {
+        return $this->render('cost_of_life/show.html.twig', [
+            'cost_of_life' => $costOfLife,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_cost_of_life_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, CostOfLife $costOfLife, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CostOfLifeType::class, $costOfLife);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cost_of_life_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('cost_of_life/edit.html.twig', [
+            'cost_of_life' => $costOfLife,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_cost_of_life_delete', methods: ['POST'])]
+    public function delete(Request $request, CostOfLife $costOfLife, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $costOfLife->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($costOfLife);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_cost_of_life_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/CostOfLifeType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CostOfLife;
+use App\Entity\Currency;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CostOfLifeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('value')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('currency', EntityType::class, [
+                'class' => Currency::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CostOfLife::class,
+        ]);
+    }
+}
