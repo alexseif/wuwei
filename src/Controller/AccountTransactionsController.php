988a71984e868cee39e61f72fac52d62ddd0dbfe
Alex Seif <alex.seif@gmail.com>
2024-12-15 13:58:05 +0200

    chore(AccountTransactions): crud

---
+++ src/Controller/AccountTransactionsController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AccountTransactions;
+use App\Form\AccountTransactionsType;
+use App\Repository\AccountTransactionsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/transactions')]
+final class AccountTransactionsController extends AbstractController
+{
+    #[Route(name: 'app_transactions_index', methods: ['GET'])]
+    public function index(AccountTransactionsRepository $accountTransactionsRepository): Response
+    {
+        return $this->render('account_transactions/index.html.twig', [
+            'account_transactions' => $accountTransactionsRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_transactions_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $accountTransaction = new AccountTransactions();
+        $form = $this->createForm(AccountTransactionsType::class, $accountTransaction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($accountTransaction);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_transactions_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('account_transactions/new.html.twig', [
+            'account_transaction' => $accountTransaction,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_transactions_show', methods: ['GET'])]
+    public function show(AccountTransactions $accountTransaction): Response
+    {
+        return $this->render('account_transactions/show.html.twig', [
+            'account_transaction' => $accountTransaction,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_transactions_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, AccountTransactions $accountTransaction, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AccountTransactionsType::class, $accountTransaction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_transactions_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('account_transactions/edit.html.twig', [
+            'account_transaction' => $accountTransaction,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_transactions_delete', methods: ['POST'])]
+    public function delete(Request $request, AccountTransactions $accountTransaction, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $accountTransaction->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($accountTransaction);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_transactions_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/AccountTransactionsType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Accounts;
+use App\Entity\AccountTransactions;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccountTransactionsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('amount')
+            ->add('note')
+            ->add('issuedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('account', EntityType::class, [
+                'class' => Accounts::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AccountTransactions::class,
+        ]);
+    }
+}
