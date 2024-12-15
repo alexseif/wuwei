36175a17f1a9e743124d555b16aea45e34b7985c
Alex Seif <alex.seif@gmail.com>
2024-12-15 15:04:10 +0200

    chore(Tasks): Tasks crud

---
+++ src/Controller/TaskListsController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\TaskLists;
+use App\Form\TaskListsType;
+use App\Repository\TaskListsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/task/lists')]
+final class TaskListsController extends AbstractController
+{
+    #[Route(name: 'app_task_lists_index', methods: ['GET'])]
+    public function index(TaskListsRepository $taskListsRepository): Response
+    {
+        return $this->render('task_lists/index.html.twig', [
+            'task_lists' => $taskListsRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_task_lists_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $taskList = new TaskLists();
+        $form = $this->createForm(TaskListsType::class, $taskList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($taskList);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_task_lists_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('task_lists/new.html.twig', [
+            'task_list' => $taskList,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_task_lists_show', methods: ['GET'])]
+    public function show(TaskLists $taskList): Response
+    {
+        return $this->render('task_lists/show.html.twig', [
+            'task_list' => $taskList,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_task_lists_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, TaskLists $taskList, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TaskListsType::class, $taskList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_task_lists_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('task_lists/edit.html.twig', [
+            'task_list' => $taskList,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_task_lists_delete', methods: ['POST'])]
+    public function delete(Request $request, TaskLists $taskList, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$taskList->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($taskList);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_task_lists_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/TaskListsType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Accounts;
+use App\Entity\TaskLists;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskListsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('status')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('account', EntityType::class, [
+                'class' => Accounts::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => TaskLists::class,
+        ]);
+    }
+}
