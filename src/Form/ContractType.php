65f4faf4ec1ed7f28e0fd8f7badd47559cac7ffc
Alex Seif <alex.seif@gmail.com>
2024-12-15 14:01:57 +0200

    chore(contract): crud

---
+++ src/Form/ContractType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use App\Entity\Contract;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContractType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('hoursPerDay')
+            ->add('startedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('isCompleted')
+            ->add('completedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('billedOn')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('client', EntityType::class, [
+                'class' => Client::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contract::class,
+        ]);
+    }
+}
