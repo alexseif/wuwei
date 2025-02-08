54ee704f2dee752588a7443f533f24d3663e2eea
Alex Seif <alex.seif@gmail.com>
2025-02-08 07:35:49 +0200

    chore: tag type refactor

--- src/Controller/TagTypeController.php
+++ src/Controller/TagTypeController.php
@@ -16,95 +16,100 @@ use Symfony\Component\Routing\Annotation\Route;
 class TagTypeController extends AbstractController
 {
 
-    #[Route('/', name: 'app_tag_type_index', methods: ['GET'])]
-    public function index(TagTypeRepository $tagTypeRepository): Response
-    {
-        return $this->render('tag_type/index.html.twig', [
-          'tag_types' => $tagTypeRepository->findAll(),
-        ]);
+  private array $twigParts = [
+    'entity_name' => 'tag_type',
+    'entity_title' => 'Tag Type'
+  ];
+
+  #[Route('/', name: 'app_tag_type_index', methods: ['GET'])]
+  public function index(TagTypeRepository $tagTypeRepository): Response
+  {
+    $this->twigParts['tag_types'] = $tagTypeRepository->findAll();
+    return $this->render('tag_type/index.html.twig', $this->twigParts);
+  }
+
+  #[Route('/new', name: 'app_tag_type_new', methods: ['GET', 'POST'])]
+  public function new(
+    Request $request,
+    EntityManagerInterface $entityManager
+  ): Response {
+    $tagType = new TagType();
+    $form = $this->createForm(TagTypeType::class, $tagType);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager->persist($tagType);
+      $entityManager->flush();
+
+      return $this->redirectToRoute(
+        'app_tag_type_index',
+        [],
+        Response::HTTP_SEE_OTHER
+      );
     }
 
-    #[Route('/new', name: 'app_tag_type_new', methods: ['GET', 'POST'])]
-    public function new(
-      Request $request,
-      EntityManagerInterface $entityManager
-    ): Response {
-        $tagType = new TagType();
-        $form = $this->createForm(TagTypeType::class, $tagType);
-        $form->handleRequest($request);
+    $this->twigParts['tag_type'] = $tagType;
+    $this->twigParts['entity'] = $tagType;
+    $this->twigParts['form'] = $form;
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($tagType);
-            $entityManager->flush();
+    return $this->render('tag_type/new.html.twig', $this->twigParts);
+  }
 
-            return $this->redirectToRoute(
-              'app_tag_type_index',
-              [],
-              Response::HTTP_SEE_OTHER
-            );
-        }
+  #[Route('/{id}', name: 'app_tag_type_show', methods: ['GET'])]
+  public function show(
+    TagType $tagType,
+    TagRepository $tagRepository
+  ): Response {
+    $this->twigParts['tag_type'] = $tagType;
+    $this->twigParts['entity'] = $tagType;
+    $this->twigParts['tags'] = $tagRepository->findBy(['tagType' => $tagType]);
 
-        return $this->render('tag_type/new.html.twig', [
-          'tag_type' => $tagType,
-          'form' => $form,
-        ]);
+    return $this->render('tag_type/show.html.twig', $this->twigParts);
+  }
+
+  #[Route('/{id}/edit', name: 'app_tag_type_edit', methods: ['GET', 'POST'])]
+  public function edit(
+    Request $request,
+    TagType $tagType,
+    EntityManagerInterface $entityManager
+  ): Response {
+    $form = $this->createForm(TagTypeType::class, $tagType);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager->flush();
+
+      return $this->redirectToRoute(
+        'app_tag_type_index',
+        [],
+        Response::HTTP_SEE_OTHER
+      );
+    }
+    $this->twigParts['tag_type'] = $tagType;
+    $this->twigParts['entity'] = $tagType;
+    $this->twigParts['form'] = $form;
+
+    return $this->render('tag_type/edit.html.twig', $this->twigParts);
+  }
+
+  #[Route('/{id}', name: 'app_tag_type_delete', methods: ['POST'])]
+  public function delete(
+    Request $request,
+    TagType $tagType,
+    EntityManagerInterface $entityManager
+  ): Response {
+    if ($this->isCsrfTokenValid(
+      'delete' . $tagType->getId(),
+      $request->request->get('_token')
+    )) {
+      $entityManager->remove($tagType);
+      $entityManager->flush();
     }
 
-    #[Route('/{id}', name: 'app_tag_type_show', methods: ['GET'])]
-    public function show(
-      TagType $tagType,
-      TagRepository $tagRepository
-    ): Response {
-        return $this->render('tag_type/show.html.twig', [
-          'tag_type' => $tagType,
-          'tags' => $tagRepository->findBy(['tagType' => $tagType]),
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_tag_type_edit', methods: ['GET', 'POST'])]
-    public function edit(
-      Request $request,
-      TagType $tagType,
-      EntityManagerInterface $entityManager
-    ): Response {
-        $form = $this->createForm(TagTypeType::class, $tagType);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute(
-              'app_tag_type_index',
-              [],
-              Response::HTTP_SEE_OTHER
-            );
-        }
-
-        return $this->render('tag_type/edit.html.twig', [
-          'tag_type' => $tagType,
-          'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_tag_type_delete', methods: ['POST'])]
-    public function delete(
-      Request $request,
-      TagType $tagType,
-      EntityManagerInterface $entityManager
-    ): Response {
-        if ($this->isCsrfTokenValid(
-          'delete' . $tagType->getId(),
-          $request->request->get('_token')
-        )) {
-            $entityManager->remove($tagType);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute(
-          'app_tag_type_index',
-          [],
-          Response::HTTP_SEE_OTHER
-        );
-    }
-
+    return $this->redirectToRoute(
+      'app_tag_type_index',
+      [],
+      Response::HTTP_SEE_OTHER
+    );
+  }
 }
