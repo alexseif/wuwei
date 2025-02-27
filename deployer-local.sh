9236d81bf1360ace1137e2177b773be6539f60f4
Alex Seif <alex.seif@gmail.com>
2025-02-27 18:03:00 +0200

    chore: new laptop installation

--- deployer-local.sh
+++ deployer-local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git pull origin master
-php8.2 /usr/local/bin/composer dump-env dev
-php8.2 /usr/local/bin/composer install
-APP_ENV=dev APP_DEBUG=1 php8.2 bin/console cache:clear --env=dev
-php8.2 bin/console doctrine:migrations:migrate --no-interaction --env=dev
+php /usr/local/bin/composer dump-env dev
+php /usr/local/bin/composer install
+APP_ENV=dev APP_DEBUG=1 php bin/console cache:clear --env=dev
+php bin/console doctrine:migrations:migrate --no-interaction --env=dev
 npm install
 ./node_modules/.bin/encore dev
