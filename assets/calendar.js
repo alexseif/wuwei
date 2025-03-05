1aeb402fec980bd475caa2176ad1ae91f946eb3b
Alex Seif <alex.seif@gmail.com>
2025-03-05 08:52:26 +0200

    chore: calendar day starts with today

--- assets/calendar.js
+++ assets/calendar.js
@@ -9,6 +9,12 @@ import listPlugin from '@fullcalendar/list';
 document.addEventListener('DOMContentLoaded', function () {
     const eventsData = document.getElementById('events-data').textContent;
     const events = JSON.parse(eventsData); // Retrieve events from the script tag
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(today.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
 
     var calendarEl = document.getElementById('calendar');
     var calendar = new Calendar(calendarEl, {
@@ -18,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
         },
-        initialDate: '2025-02-12',
+        initialDate: formattedDate,
         initialView: 'timeGridWeek',
         events: events,
         navLinks: true, // can click day/week names to navigate views
